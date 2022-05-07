2f25fcdee747b655c85a7960aac9402676712366
Craig Bloodworth <craig.bloodworth@theinformationlab.co.uk>
2022-05-07 22:41:48 +0100

    Cleanup

--- connector/tmp/MyFetcher.js
+++
@@ -1,9 +0,0 @@
-
-
-export default class MyFetcher extends Fetcher {
-  async fetch(request, context) {
-    return await AjaxRequest.getJSON(request.data.url)
-  }
-}
-
-				globalRefs["MyFetcher"] = MyFetcher
